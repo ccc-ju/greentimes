45de784927d1783354fefa46497fa8f8c4a1ff3b
clp <ccc-ju@outlook.com>
2019-10-31 11:19:16 +0800

    clp

--- gt/src/App.vue
+++ gt/src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
     <router-view/>
-    <h1>1</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped="">
+
+#app{
+  height: 100vh;
+  width: 100vw;
 }
+body{
+  margin: 0;
+  padding: 0;
+   height: 100vh;
+  width: 100vw;
+}
+
 </style>
---
+++ gt/src/views/BerForget.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="box">
+    <van-nav-bar title="忘记密码" left-arrow></van-nav-bar>
+    <div class="from">
+      <van-cell-group class="name">
+        <van-field v-model="value" center clearable label="手机号码" placeholder="请输入手机号码" />
+      </van-cell-group>
+      <van-cell-group class="sms">
+        <van-field v-model="sms" center clearable label="验证码" placeholder="请输入验证码">
+          <van-button slot="button" size="small" type="primary" class="yzm">获取验证码</van-button>
+        </van-field>
+      </van-cell-group>
+      <van-button type="primary" v-tap="{methods:regist}" class="conf">确认</van-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Toast } from "vant";
+export default {
+  name: "BerForget",
+  data() {
+    return {
+      value:'',
+      sms:''
+    };
+  },
+  methods: {
+    regist() {
+      this.$router.push("/BerForget");
+    }
+  }
+};
+</script>
+
+<style scoped="">
+.box {
+  width: 100vw;
+  height: 100vh;
+  background: #f2f2f2;
+}
+.conf {
+  width: 90vw;
+  margin-top: 3vh;
+  background: rgba(0, 255, 0, 0);
+  border-radius: 5px;
+  border: 0;
+  margin: 2vh 0 0 5vw;
+  background: #2bb32b;
+}
+.name,.sms{
+  margin-top: 1.5vh;
+}
+
+
+</style>

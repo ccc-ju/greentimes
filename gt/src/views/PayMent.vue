62f02393a89de8d47f7a55dec60985287b1df8f0
dwg <2430856943@qq.com>
2019-11-05 21:34:19 +0800

    dwg

--- gt/src/views/PayMent.vue
+++ gt/src/views/PayMent.vue
@@ -34,12 +34,12 @@ export default {
 
 <style scoped="">
 .wrap {
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 }
 .wrap img {
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
 }
 span{
   display: block;
